3bb5062e44ce637c9ea0769abe91362373d2d743
Yonis7 <yonisaden7@gmail.com>
2023-02-14 11:38:13 +0000

    eqArray test has been fixed

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,13 @@
 
 const eqArrays = require('../eqArrays.js')
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+
+describe ('Test if eqArrays function works', () => {
+  
+  it(`[1, 2, 3] should = [1, 2, 3]`, () => {
+    const result = eqArrays([1, 2, 3], [1, 2, 3]);
+    assert.strictEqual(result, true,);
+  });
+});
